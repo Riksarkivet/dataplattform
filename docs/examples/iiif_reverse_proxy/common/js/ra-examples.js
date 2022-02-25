2acea6fa6eca42bb0a1b789cf339433b1c015e87
Nils Weinander <nils.weinander@kulturit.se>
2022-02-25 16:01:42 +0100

    IIIF reverse proxy and collection browsing example
    WIP

--- docs/examples/iiif_reverse_proxy/common/js/ra-examples.js
+++ docs/examples/iiif_reverse_proxy/common/js/ra-examples.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const TOP_COLLECTION_URL = "/iiif/collection/riksarkivet";
+const IIIF_COLLECTION_BASE_URL = "/iiif/collection/";
+const IIIF_COLLECTION_BASE_ACC_URL = "/iiif-acc/collection/";
+const TOP_COLLECTION_ID = "riksarkivet";
 
 const getCollection = async (uri) => {
 	console.log(`--- Getting ${uri} ---`);
@@ -8,3 +10,14 @@ const getCollection = async (uri) => {
 	return (await fetch(uri)).json();
 }
 
+const collectionUrl = () => {
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	const params = Object.fromEntries(urlSearchParams.entries());
+	const urlPart = params.arkiv ? `arkiv/${params.arkiv}` : TOP_COLLECTION_ID;
+	const url = `${IIIF_COLLECTION_BASE_ACC_URL}${urlPart}`;
+	return url;
+}
+
+const itemLink = (item) => {
+	return `${item.label.sv[0]}  <span class='oi' data-glyph='${item.type === "Collection" ? "arrow-right" : "eye"}' title='Link icon' aria-hidden='true'></span>`
+}
