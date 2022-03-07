c467a1b5f4bb15679fef2f56876f388d7e748bd2
Nils Weinander <nils.weinander@kulturit.se>
2022-03-07 17:08:36 +0100

    IIIF Proxy example
    Better Alpine data handling

--- docs/examples/iiif_reverse_proxy/common/js/ra-examples.js
+++ docs/examples/iiif_reverse_proxy/common/js/ra-examples.js
@@ -4,7 +4,7 @@ const IIIF_COLLECTION_BASE_URL = "/iiif/collection/";
 const IIIF_COLLECTION_BASE_ACC_URL = "/iiif-acc/collection/";
 const TOP_COLLECTION_ID = "riksarkivet";
 
-const prod = true;
+const prod = false;
 
 const collectionPath = (uri) => {
 	const regex = /https:\/\/lbiiif(?:-acc)?.riksarkivet.se\/collection\/(.*)/;
@@ -33,4 +33,39 @@ const collectionUrl = () => {
 
 const itemLink = (item) => {
 	return `${item.label.sv[0]}  <span class='oi' data-glyph='${item.type === "Collection" ? "arrow-right" : "eye"}' title='Link icon' aria-hidden='true'></span>`
+}
+
+const initBrowseContext = () => {
+	Alpine.data('browseContext', () => ({
+		trail: [],
+		item: {},
+		collection: {},
+
+		toggleLoading() {
+			this.loading = !this.loading;
+		},
+  
+		async getItems() {
+			if (this.item.type === 'Collection') {
+				this.trail.push({
+					id: this.collection.id,
+					label: this.collection.label.sv[0]
+				})
+				this.loading = true;
+				this.collection = await getCollection(this.item.id)
+				this.loading = false;
+			} else {
+				this.image.present = true
+				this.image.id = this.item.id
+			}
+		},
+
+		async goBack(index) {
+			const ancestor = this.trail[index];
+			this.trail = this.trail.slice(0, index);
+			this.loading = true;
+			this.collection = await getCollection(ancestor.id);
+			this.loading = false;
+		}
+	}))
 }
