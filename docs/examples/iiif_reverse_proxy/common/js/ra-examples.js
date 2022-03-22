09081ec56ed2c6d4eb3c6ca8028540f676bfa27d
Nils Weinander <nils.weinander@kulturit.se>
2022-03-22 16:12:32 +0100

    IIIF Proxy example
    Removing acc

--- docs/examples/iiif_reverse_proxy/common/js/ra-examples.js
+++ docs/examples/iiif_reverse_proxy/common/js/ra-examples.js
@@ -1,71 +1,127 @@
 "use strict";
 
 const IIIF_BASE_URL = "/iiif/";
-const IIIF_BASE_ACC_URL = "/iiif-acc/";
 
 const IIIF_COLLECTION_BASE_URL = `${IIIF_BASE_URL}collection/`;
-const IIIF_COLLECTION_BASE_ACC_URL = `${IIIF_BASE_ACC_URL}collection/`;
 const TOP_COLLECTION_ID = "riksarkivet";
 
-const prod = false;
-
+/**
+ * Get id part of collection URI (name or arkiv/<pid>)
+ * @param {*} uri Absolute or relative URI
+ * @returns id part 
+ */
 const collectionPath = (uri) => {
 	const regex = /https:\/\/lbiiif(?:-acc)?.riksarkivet.se\/collection\/(.*)/;
 	const match = uri.match(regex);
 	return !!match && match.length > 1 ? match[1] : uri;
 }
 
+/**
+ * Get collection resource (JSON-LD). 
+ * NB: this is very simplistic, without error handling
+ * @param {*} uri Collection URI
+ * @returns Resource in JSON-LD format
+ */
 const getCollection = async (uri) => {
 	// For absolute URIs (including https://lbiiif.riksarkivet.se), extract the id part
-	const url = uri.startsWith("https")
-		? (prod ? IIIF_COLLECTION_BASE_URL : IIIF_COLLECTION_BASE_ACC_URL) + collectionPath(uri)
-		: uri;
+	const url = uri.startsWith("https") ? `${IIIF_COLLECTION_BASE_URL}${collectionPath(uri)}` : uri;
 	console.log(`--- Getting ${uri} by way of ${url} ---`);
 
 	return (await fetch(url)).json();
 }
 
-const manifestPath = (uri) => {
+/**
+ * Get id part of manifest URI
+ * @param {*} uri Absolute or relative URI
+ * @returns id part 
+ */
+ const manifestPath = (uri) => {
 	const regex = /https:\/\/lbiiif(?:-acc)?.riksarkivet.se\/(.*)/;
 	const match = uri.match(regex);
 	return !!match && match.length > 1 ? match[1] : uri;
 }
 
-const getManifest = async (uri) => {
+/**
+ * Get manifest resource (JSON-LD). 
+ * NB: this is very simplistic, without error handling
+ * @param {*} uri Manifest URI
+ * @returns Resource in JSON-LD format
+ */
+ const getManifest = async (uri) => {
 	// For absolute URIs (including https://lbiiif.riksarkivet.se), extract the id part
-	const url = uri.startsWith("https")
-		? (prod ? IIIF_BASE_URL : IIIF_BASE_ACC_URL) + manifestPath(uri)
-		: uri;
+	const url = uri.startsWith("https") ? `${IIIF_BASE_URL}${manifestPath(uri)}` : uri;
 	console.log(`--- Getting ${uri} by way of ${url} ---`);
 
 	return (await fetch(url)).json();
 }
 
+/**
+ * Get collection proxy URL, from query parameter ?arkiv if present, top collection id otherwise
+ * @returns Proxy URL
+ */
 const collectionUrl = () => {
 	const urlSearchParams = new URLSearchParams(window.location.search);
 	const params = Object.fromEntries(urlSearchParams.entries());
 	const urlPart = params.arkiv ? `arkiv/${params.arkiv}` : TOP_COLLECTION_ID;
-	const url = `${prod ? IIIF_COLLECTION_BASE_URL : IIIF_COLLECTION_BASE_ACC_URL}${urlPart}`;
+	const url = `${IIIF_COLLECTION_BASE_URL}${urlPart}`;
 	return url;
 }
 
+/**
+ * Get IIIF image proxy URL
+ * @param {*} uri Image URI
+ * @param {*} v2 Use IIIF Image v2?
+ * @returns Proxy URL
+ */
 const imageUrl = (uri, v2 = false) => {
 	const regex = /https:\/\/lbiiif(?:-acc)?.riksarkivet.se\/(.*)/;
 	const match = uri.match(regex);
 	return !!match && match.length > 1 
-		? `${prod ? IIIF_BASE_URL : IIIF_BASE_ACC_URL}${v2 ? "v2/" : ""}${match[1]}`
+		? `${IIIF_BASE_URL}${v2 ? "v2/" : ""}${match[1]}`
 		: uri;
 }
 
+/**
+ * Get HTML for link to collection or image presentation
+ * @param {*} item Item object
+ * @returns HTML for link component
+ */
 const itemLink = (item) => {
 	return `${item.label.sv[0]}  <span class='oi' data-glyph='${item.type === "Collection" ? "arrow-right" : "eye"}' title='Link icon' aria-hidden='true'></span>`;
 }
 
-const backLink = (prev) => {
+/**
+ * Get HTML for back link to parent collection
+ * @param {*} prev Item object
+ * @returns HTML for back link component
+ */
+ const backLink = (prev) => {
 	return `${prev.label} <span class='oi' data-glyph='arrow-left' title='Arrow left' aria-hidden='true'></span>`;
 }
 
+const stepImage = (imageIndex, forward) => {
+	const store = Alpine.store("browseContext")
+	if (forward) {
+		if (imageIndex < store.image.urls.length - 1) {
+			imageIndex++;
+		} else {
+			imageIndex = 0;
+		} 
+	} else {
+		if (imageIndex > 0) {
+			imageIndex--;
+		} else {
+			imageIndex = store.image.urls.length - 1;
+		}
+	}
+}
+
+/**
+ * Alpine.js initialization
+ */
 const initBrowseContext = () => {
+	// Use store rather than data in order to access state from the browser
+	// navigation handler
 	Alpine.store("browseContext", {
 		loading: false,
 		trail: [],
@@ -103,7 +159,7 @@ const initBrowseContext = () => {
 		 * Handler for browser back/forward event. NB: only handles back, forward is
 		 * too complex for this simple example
 		 * 
-		 * @param {*} event 
+		 * @param {*} event Navigation event
 		 */
 		async handleNavigation(event) {
 			if (event.state) {
@@ -117,6 +173,7 @@ const initBrowseContext = () => {
 		}
 	});
 
+	// And some data too, for state which is only used in the collection browsing
 	Alpine.data("browseContext", () => ({
 		item: {},
 
@@ -139,15 +196,6 @@ const initBrowseContext = () => {
 				store.image.id = this.item.id;
 				const manifest = await getManifest(this.item.id);
 				store.image.urls = manifest.items.map(element => imageUrl(element.items[0].items[0].body.id, true));
-				// const tileSources = store.image.urls.map(url => ({
-				// 	type: "image",
-				// 	url: url
-				// }));
-				// const viewer = OpenSeadragon({
-				// 	id: "viewer",
-				// 	prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon/build/openseadragon/images/",
-				// 	tileSources: tileSources
-				// });
 			}
 		}
 	}));
