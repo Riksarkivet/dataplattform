970fd97d10cb7f395768f24154ab6112c497577d
Nils Weinander <nils.weinander@kulturit.se>
2022-03-25 14:09:01 +0100

    IIIF Presentation examples
    Get manifest URI from archival record persistent id

--- docs/examples/iiif_presentation/pid_to_image_id.py
+++ docs/examples/iiif_presentation/pid_to_image_id.py
@@ -1,2 +1,13 @@
 import requests
+import sys
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        res = requests.get(f'https://lbiiif.riksarkivet.se/collection/arkiv/{sys.argv[1]}')
+        if res.status_code == 200:
+            collection = res.json()
+            print(collection.get("items", [{}])[0].get("id", ""))
+        else:
+            print(f'Error: {res.status_code}')
+    else:
+        print("Usage: python pid_to_image_id.py <pid>")
