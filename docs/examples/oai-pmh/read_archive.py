2ebbafbe36ce5f40d5445f187ae0f869f4721d75
Nils Weinander <nils.weinander@riksarkivet.se>
2022-06-29 16:52:26 +0200

    Exempel OAI-PMH
    Läsa in EAD
    WIP

--- docs/examples/oai-pmh/read_archive.py
+++ docs/examples/oai-pmh/read_archive.py
@@ -8,7 +8,8 @@ def base_data(ead, ref, archive_data):
     unitid = ref.find('./{*}unitid')
     archive_data['referenskod'] = unitid.text
     unittitle = ref.find('./{*}unittitle')
-    archive_data['titel'] = unittitle.text
+    if unittitle is not None:
+        archive_data['titel'] = unittitle.text
     unitdate = ref.find('./{*}unitdate')
     if unitdate is not None:
         archive_data['datering'] = unitdate.text
@@ -16,6 +17,23 @@ def base_data(ead, ref, archive_data):
     url = url_ref.attrib.get('{http://www.w3.org/1999/xlink}href')
     archive_data['persistent_id'] = url.split('/')[-1]
     archive_data['url'] = url
+    for link in ead.findall('./{*}archdesc/{*}did/{*}dao'):
+        role = link.attrib.get('{http://www.w3.org/1999/xlink}role')
+        if role == 'IMAGE':
+            archive_data['bildvisning'] = link.attrib.get('{http://www.w3.org/1999/xlink}href')
+        elif role == 'MANIFEST':
+            archive_data['iiif_manifest'] = link.attrib.get('{http://www.w3.org/1999/xlink}href')
+
+def institution_data(ref, archive_data):
+    eadid = ref.find('./{*}eadid')
+    if eadid is not None:
+        institution = eadid.attrib.get('mainagencycode')
+        if institution is not None:
+            institution = institution.replace('-', '/')
+            archive_data['arkivinstitution'] = {
+                'referenskod': institution,
+                'url': f'https://sok.riksarkivet.se/nad?postid=ArkisRef+{urllib.parse.quote_plus(institution)}&type=2&s=balder'
+            }
 
 
 def origin_data(ref, archive_data):
@@ -36,32 +54,62 @@ def extent_data(ref, archive_data):
         archive_data['omfång'] = f'{extent.text} {extent.attrib.get("unit")}'
 
 
-def restrict_data(ref, archive_data):
+def restrict_data(ref, key, archive_data):
     if ref is not None:
         data = {}
-        data['omfattning'] = ref.attrib.get('type')
+        specification = ref.attrib.get('type')
+        if specification is not None:
+            data['omfattning'] = specification
         note = ref.find('./{*}p')
         if note is not None:
-            data['anmärkning'] = note.text
-        archive_data['villkor'] = data
+            if note.text is not None:
+                data['anmärkning'] = note.text
+            else: 
+                extref = note.find('./{*}extref')
+                if extref is not None:
+                    data['anmärkning'] = extref.attrib.get('{http://www.w3.org/1999/xlink}href')
+        all_restrict = archive_data.get('villkor')
+        if all_restrict is None:
+            all_restrict = {}
+            archive_data['villkor'] = all_restrict
+        all_restrict[key] = data
 
+
+def unit_data(ead, unit_root, target_data):
+    did = unit_root.find('./{*}did')
+    if did is not None:
+        base_data(ead, did, target_data)
+        header = ead.find('./{*}eadheader')
+        institution_data(header, target_data)
+        origin_data(did, target_data)
+        extent_data(did, target_data)
+    restrict = archdesc.find('./{*}accessrestrict')
+    restrict_data(restrict, 'åtkomst', target_data)
+    restrict = archdesc.find('./{*}userestrict')
+    restrict_data(restrict, 'användning', target_data)
+    target_data['innehåll'] = []
+    for child in unit_root.findall('./{*}dsc/{*}c'):
+        child_data = {}
+        unit_data(ead, child, child_data)
+        target_data['innehåll'].append(child_data)
+    for child in unit_root.findall('./{*}c'):
+        child_data = {}
+        unit_data(ead, child, child_data)
+        target_data['innehåll'].append(child_data)
+    if len(target_data['innehåll']) == 0:
+        del(target_data['innehåll'])
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         ref_code = urllib.parse.quote_plus(sys.argv[1])
-        res = requests.get(f'https://oai-pmh.riksarkivet.se/OAI?verb=GetRecord&identifier={ref_code}&metadataPrefix=oai_ape_ead')
+        res = requests.get(f'https://oai-pmh-acc.riksarkivet.se/OAI?verb=GetRecord&identifier={ref_code}&metadataPrefix=oai_ape_ead')
         if res.status_code == 200:
             archive_data = {}
             tree = ET.fromstring(res.content)
             ead = tree.find('.//{*}ead')
-            did = ead.find('./{*}archdesc/{*}did')
-            if did is not None:
-                base_data(ead, did, archive_data)
-                origin_data(did, archive_data)
-                extent_data(did, archive_data)
-            restrict = ead.find('./{*}archdesc/{*}userestrict')
-            restrict_data(restrict, archive_data)
-            print(json.dumps(archive_data, indent=3))
+            archdesc = ead.find('./{*}archdesc')
+            unit_data(ead, archdesc, archive_data)
+            print(json.dumps(archive_data, indent=3, ensure_ascii=False))
         else:
             print(f'Error: {res.status_code}')
     else:
